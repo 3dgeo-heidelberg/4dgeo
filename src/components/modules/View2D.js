9fb0d0e9de9766e12417178f11a800946832196d
momueller2 <mo.mueller2@web.de>
2025-05-14 11:40:38 +0200

    Added the clustering functionality for the 2d map

--- src/components/modules/View2D.js
+++ src/components/modules/View2D.js
@@ -1,25 +1,29 @@
-import { useEffect, useState } from "react";
-import { MapContainer, ImageOverlay, LayersControl, Polygon, Rectangle, Circle, LayerGroup, Tooltip } from "react-leaflet"; // Import Leaflet components for rendering the map and layers
+import { MapContainer, useMapEvents, ImageOverlay, LayersControl, Polygon, Popup, Rectangle, Circle, LayerGroup, Tooltip, Marker, useMapEvent } from "react-leaflet"; // Import Leaflet components for rendering the map and layers
 import L from "leaflet"; // Import Leaflet library to access its utility methods
-import "leaflet/dist/leaflet.css"; // Import Leaflet's CSS for map styling
+
+import "leaflet/dist/leaflet.css";
+import 'react-leaflet-markercluster/styles'
+import * as turf from "@turf/turf";
+import MarkerClusterGroup from "react-leaflet-markercluster";
 
 export default function View2D({ 
     observations,
-    typeColors // Object mapping event types to colors for styling
+    typeColors
  }) {
-    const [backgroundImageData, setBackgroundImageData] = useState("")
+    let backgroundImageData = null;
+    if(observations.find(observation => observation.backgroundImageData)) {
+        backgroundImageData = observations.find(observation => observation.backgroundImageData).backgroundImageData
+    }
 
-    useEffect(() => {
-        const imgData = observations.find(observation => observation.backgroundImageData)
-        if(imgData) {
-            setBackgroundImageData(imgData.backgroundImageData);
-        }
-    }, [observations]);
+    
 
     const getEventGeometries = () => {
         return Array.from(observations).map((observation, i) => Array.from(observation.geoObjects).map((geoObject, j) => {
             switch(geoObject.geometry.type) {
                 case 'Polygon':
+                    let polygon = turf.polygon([geoObject.geometry.coordinates]);
+                    let centroid = turf.centroid(polygon);
+
                     return (
                         <Polygon
                             key={`${i}-${j}`} 
@@ -30,6 +34,7 @@ export default function View2D({
                                 fillOpacity: 0.65
                             }}
                             positions={geoObject.geometry.coordinates}
+                            center={centroid.geometry.coordinates}
                         >
                             <Tooltip>
                                 <b>Type:</b> {geoObject.type}
@@ -39,25 +44,29 @@ export default function View2D({
                                     )
                                 })}
                             </Tooltip>
-                        </Polygon>              
+                        </Polygon>
                     );
                 case 'Point':
                     return (
                         <Circle 
                             key={`${i}-${j}`}
-                            color={typeColors.get(geoObject.type)}
+                            color={"purple"}
                             weight={1} // Line thickness
                             opacity={1} // Line opacity
                             fillOpacity={0.65} // Fill opacity
                             center={geoObject.geometry.coordinates[0]}
-                            radius={5}                            
+                            radius={4}                            
                             crs={L.CRS.Simple}
                         >
                             <Tooltip>
                                 <b>Type:</b> {geoObject.type}
+                                {geoObject.customEntityData ? Object.entries(geoObject.customEntityData).map((item, i) => {
+                                    return (
+                                        <div key={i}><b>{item[0]}:</b> {item[1]}</div>
+                                    )
+                                }) : ""}
                             </Tooltip>
-                        </Circle>
-                        
+                        </Circle>                        
                     );
                 case 'Rectangle':
                     return (
@@ -75,32 +84,41 @@ export default function View2D({
                 default:
                     return "";
             }
-        }));
+        })).flat();
     };
+
+    let geoObjectGeometries = getEventGeometries();
 
     return backgroundImageData ? (
         <MapContainer
-            center={[0, 0]} // Center the map on the image
+            center={[-(backgroundImageData.height/2), backgroundImageData.width/2]}
             crs={L.CRS.Simple}
-            style={{ height: "100%", width: "100%" }} // Make the map take up full space
-            minZoom={-2} // Set initial zoom level
+            style={{ height: "100%", width: "100%" }}
+            minZoom={-2}
+            maxZoom={5}
             zoom={-2}
-            scrollWheelZoom={true} // Disable zooming with the mouse wheel
+            scrollWheelZoom={true}
         >
+            {/* <MapEventHandlers onMoveEnd={updateMap}/> */}
+            <ImageOverlay
+                url={backgroundImageData.url}
+                bounds={
+                    [[(-backgroundImageData.height), 0],
+                    [0, backgroundImageData.width]]
+                }
+                opacity={0.9}
+            />
             <LayersControl position="topright">
-                <LayersControl.BaseLayer checked name="Image Layer">
-                    <ImageOverlay
-                        url={backgroundImageData.url}
-                        bounds={
-                            [[(-backgroundImageData.height), 0],
-                            [0, backgroundImageData.width]]
-                        }
-                        opacity={0.9}
-                    />
-                </LayersControl.BaseLayer>
-                <LayersControl.Overlay checked name="Geoobjects">
+                <LayersControl.Overlay checked name="Clustered geoobjects">
                     <LayerGroup>
-                        {getEventGeometries()}
+                        <MarkerClusterGroup>
+                            {geoObjectGeometries}
+                        </MarkerClusterGroup>
+                    </LayerGroup>
+                </LayersControl.Overlay>    
+                <LayersControl.Overlay name="Nonclustered geoobjects">
+                    <LayerGroup>
+                        {geoObjectGeometries}
                     </LayerGroup>
                 </LayersControl.Overlay>
             </LayersControl>
