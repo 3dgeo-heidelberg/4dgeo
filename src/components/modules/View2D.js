fd6f9b37ba27e6130fe2b80130d9d9cef0ac3fdf
momueller2 <mo.mueller2@web.de>
2025-06-07 00:52:15 +0200

    Initial load now starts with first observation. Also the slider can be range or single now

--- src/components/modules/View2D.js
+++ src/components/modules/View2D.js
@@ -1,15 +1,22 @@
 import { MapContainer, ImageOverlay, LayersControl, Polygon, Rectangle, Circle, LayerGroup, Tooltip } from "react-leaflet"; // Import Leaflet components for rendering the map and layers
-import L from "leaflet"; // Import Leaflet library to access its utility methods
+import L, { layerGroup } from "leaflet"; // Import Leaflet library to access its utility methods
 
 import "leaflet/dist/leaflet.css";
 import 'react-leaflet-markercluster/styles'
-import * as turf from "@turf/turf";
-import MarkerClusterGroup from "react-leaflet-markercluster";
+import { useRef } from "react";
+import 'leaflet.markercluster';
+import 'Leaflet.Deflate'
+import { clusterEach } from "@turf/turf";
+
 
 export default function View2D({ 
     observations,
     typeColors
  }) {
+    const clusteredLayer = useRef(null);
+    const normalLayer = useRef(null);
+    const deflatedLayer = useRef(null);
+
     let backgroundImageData = null;
     if(observations.find(observation => observation.backgroundImageData)) {
         backgroundImageData = observations.find(observation => observation.backgroundImageData).backgroundImageData
@@ -54,82 +61,89 @@ export default function View2D({
         }
     }
 
-    const getGeoObjectGeometries = () => {
-        return Array.from(observations).map((observation, i) => Array.from(observation.geoObjects).map((geoObject, j) => {
-            switch(geoObject.geometry.type) {
-                case 'Polygon':
-                    let polygon = turf.polygon([geoObject.geometry.coordinates]);
-                    let centroid = turf.centroid(polygon);
+    L.polygonClusterable = L.Polygon.extend({
+        _originalInitialize: L.Polygon.prototype.initialize,
 
-                    return (
-                        <Polygon
-                            key={`${i}-${j}`} 
-                            pathOptions={{ 
+        initialize: function (bounds, options) {
+            this._originalInitialize(bounds, options);
+            this._latlng = this.getBounds().getCenter(); // Define the polygon "center".
+        },
+
+        getLatLng: function () {
+            return this._latlng;
+        },
+
+        // dummy method.
+        setLatLng: function () {}
+    })
+
+    const getGeoObjectGeometries = () => {
+        if (clusteredLayer.current && normalLayer.current) {
+            clusteredLayer.current.clearLayers();
+            normalLayer.current.clearLayers();
+            
+            console.log("rendering: ", observations)
+            var objectsToCluster = L.markerClusterGroup();
+
+
+            var deflateFeatures = L.deflate({minSize: 20, markerLayer: objectsToCluster});
+            deflateFeatures.addTo(clusteredLayer.current);
+            // objectsToCluster.addTo(clusteredLayer.current);
+            Array.from(observations).map((observation, i) => Array.from(observation.geoObjects).map((geoObject, j) => {
+                switch(geoObject.geometry.type) {
+                    case 'Polygon':
+                        console.log("Polygon")
+                        const leafletPolygon = new L.polygonClusterable(
+                            geoObject.geometry.coordinates, {
                                 color: typeColors.get(geoObject.type) || "black",
                                 weight: 2,
                                 opacity: 1,
-                                fillOpacity: 0.65
-                            }}
-                            positions={geoObject.geometry.coordinates}
-                            center={centroid.geometry.coordinates}
-                        >
-                            <Tooltip>
-                                <b>Type:</b> {geoObject.type}
-                                {Object.entries(geoObject.customEntityData).map((item, i) => {
-                                    return (
-                                        <div key={i}><b>{item[0]}:</b> {item[1]}</div>
-                                    )
-                                })}
-                            </Tooltip>
-                        </Polygon>
-                    );
-                case 'Point':
-                    return (
-                        <Circle 
-                            key={`${i}-${j}`}
-                            pathOptions={{
-                                color: typeColors.get(geoObject.type) ? typeColors.get(geoObject.type) : "black",
+                                fillOpacity: 0.65,
+                                crs: L.CRS.Simple
+                            }
+                        ).bindTooltip(
+                            `<b>Type:</b> ${geoObject.type}<br>` +
+                            (geoObject.customEntityData ? Object.entries(geoObject.customEntityData).map((item, i) => {
+                                return "<div key={" + i + "}><b>" + item[0] + ":</b> " + item[1] + "</div>";
+                            }) : ""));
+                        // leafletPolygon.addTo(normalLayer.current);
+                        // objectsToCluster.addLayer(leafletPolygon);
+                        leafletPolygon.addTo(deflateFeatures);
+                        // leafletPolygon.addTo(layerToDeflate);
+                        return;
+                    case 'Point':
+                        console.log("point")
+                        const leafletCircle = L.circle(
+                            geoObject.geometry.coordinates[0], {
+                                color: typeColors.get(geoObject.type) || "black",
                                 weight: 1,
                                 opacity: 1,
                                 fillOpacity: 0.65,
-                            }}
-                            weight={1}
-                            opacity={1}
-                            fillOpacity={0.65}
-                            center={geoObject.geometry.coordinates[0]}
-                            radius={4}                            
-                            crs={L.CRS.Simple}
-                        >
-                            <Tooltip>
-                                <b>Type:</b> {geoObject.type}
-                                {geoObject.customEntityData ? Object.entries(geoObject.customEntityData).map((item, i) => {
-                                    return (
-                                        <div key={i}><b>{item[0]}:</b> {item[1]}</div>
-                                    )
-                                }) : ""}
-                            </Tooltip>
-                        </Circle>                        
-                    );
-                case 'Rectangle':
-                    return (
-                        <Rectangle
-                            key={`${i}-${j}`}
-                            pathOptions={{ 
-                                color: typeColors.get(geoObject.type) || "black",
-                                weight: 2,
-                                opacity: 1,
-                                fillOpacity: 0.5,
-                            }}
-                            positions={geoObject.geometry.coordinates}
-                        />
-                    );
-                default:
-                    return "";
-            }
-        })).flat();
+                                radius: 4,
+                                crs: L.CRS.Simple
+                            }
+                        ).bindTooltip(
+                            `<b>Type:</b> ${geoObject.type}<br>` +
+                            (geoObject.customEntityData ? Object.entries(geoObject.customEntityData).map((item, i) => {
+                                return "<div key={" + i + "}><b>" + item[0] + ":</b> " + item[1] + "</div>";
+                            }) : "")
+                        ).addTo(normalLayer.current);
+                        leafletCircle.addTo(objectsToCluster);
+                        // objectsToCluster.addLayer(leafletCircle);
+                        return;
+                }
+            }));
+
+            // objectsToCluster.addTo(clusteredLayer.current);
+            // layerToDeflate.addTo(deflatedLayer.current);
+            
+        }
     };
 
-    let geoObjectGeometries = getGeoObjectGeometries();
+    if(clusteredLayer.current && normalLayer.current) {
+        getGeoObjectGeometries();
+    }
+    
 
     return backgroundImageData ? (
         <MapContainer
@@ -152,17 +166,14 @@ export default function View2D({
             />
             <LayersControl position="topright">
                 <LayersControl.Overlay checked name="Clustered geoobjects">
-                    <LayerGroup>
-                        <MarkerClusterGroup>
-                            {geoObjectGeometries}
-                        </MarkerClusterGroup>
-                    </LayerGroup>
-                </LayersControl.Overlay>    
-                <LayersControl.Overlay name="Nonclustered geoobjects">
-                    <LayerGroup>
-                        {geoObjectGeometries}
-                    </LayerGroup>
+                    <LayerGroup ref={clusteredLayer} />
                 </LayersControl.Overlay>
+                <LayersControl.Overlay name="Nonclustered geoobjects">
+                    <LayerGroup ref={normalLayer} />
+                </LayersControl.Overlay>
+                {/* <LayersControl.Overlay name="Deflated geoobjects">
+                    <LayerGroup ref={deflatedLayer} />
+                </LayersControl.Overlay> */}
             </LayersControl>
         </MapContainer>
     ) : (
